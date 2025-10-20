81404633ce077e131b3de2287dce892580e058e0
Vedran Skvorc <skvorcvedran@gmail.com>
2025-10-20 10:31:44 +0200

    Initial commit

---
+++ point.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
